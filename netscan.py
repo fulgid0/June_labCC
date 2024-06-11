64b3a129b3164cb8423ed74b6f92de5e9c9f5b27
fulgid0 <118464641+fulgid0@users.noreply.github.com>
2024-06-11 14:00:04 +0200

    Update netscan.py
    Funzionalita' di portscanning inserita

--- netscan.py
+++ netscan.py
@@ -15,6 +15,9 @@ ip_model = re.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}(
 MAX_Port = 65535
 My_IP=str(os.popen('hostname -I').read()) #Acquisisco l'IP della macchina su cui gira il python
 exception_list = []
+TARGET = ""
+open_ports = []
+
 
 def test_port (IP, port):
  time.sleep(1)
@@ -34,6 +37,26 @@ def ip_exception (): #scorrimento delle eventuali eccezioni per verifica di conf
   for i, ip_test in enumerate(sys.argv[2:], start=2):
     ip_check(ip_test,i)
 
+def run_ping(sample):
+ response = os.system('ping -c 1 -w 1 '+sample)
+ if response == 0:
+  time.sleep(5)
+  return(sample)
+ else:
+  return(0)
+
+def PING_sweep(subnet_root):
+ i = 0
+ while i in range (255):
+  sample=subnet_root+str(i)
+  if sample not in exception_list:  #controllo se l'ip e tra quelli esclusi
+    result=run_ping(sample) #testo via ping
+    if result != 0:
+     return result
+  else:
+   print(sample+" skippato come da input CLI")
+  i = i+1
+
 try:
  subnet = sys.argv[1] #controllo se mi e' stato fornito almeno un argomento via CLI
 except:
@@ -51,6 +74,22 @@ ip_exception()
 '''INIZIO SCANSIONE DELLA RETE'''
 
 subnet_root = ".".join(subnet.split('.')[:-1])+'.' # ritorna la parte della subnet nel formato 192.168.1. in modo da poter fare il for in maniera semplice
-PING_sweep(subnet_root)
+TARGET = PING_sweep(subnet_root)
+print("TROVATO TARGET ALL'IP: "+TARGET)
+
+'''INIZIO PORT SCAN'''
+
+print("\n\n\n-----------------")
+print("-PORT SCAN SU TARGET "+TARGET+"-")
 
 
+for port in range(0, MAX_Port):
+    try:
+     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.settimeout(1)
+      s.connect((TARGET, port))
+      open_ports.append(port)
+      print ("OPEN PORT FOUND ["+str(port)+"]")
+    except:
+     pass
+     #print (str(port)+ " CHIUSA")
