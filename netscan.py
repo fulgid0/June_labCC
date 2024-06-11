5fa6520f82445dd6a4381c3e0c45f578109f2e07
fulgid0 <118464641+fulgid0@users.noreply.github.com>
2024-06-11 09:35:10 +0200

    Create netscan.py
    solo i print inziali

---
+++ netscan.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+import socket
+
+print('Simple python portscan. First find all the interfaces on the network not associated on the running system, then do a port scan. It olny manage /24 subnets.')
+
+print ('USAGE: python netscan.py <IP>')
